605d8eb42b8d094153f9f8ab17a6ff6c67a9e7f1
Edward Mills <59481141+EdwardPMillsJr@users.noreply.github.com>
2021-03-08 21:23:52 -0500

    Add files via upload

---
+++ CameraController.cs
@@ -0,0 +1,25 @@
+﻿using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    private float sensitivity = 11f;
+
+    private CinemachineComposer composer;
+  
+    private void Start()
+    {
+        composer = GetComponent<CinemachineVirtualCamera>().GetCinemachineComponent<CinemachineComposer>();
+    }
+
+ 
+    private void Update()
+    {
+        float vertical = Input.GetAxis("Mouse Y") * sensitivity;
+        composer.m_TrackedObjectOffset.y += vertical;
+        composer.m_TrackedObjectOffset.y = Mathf.Clamp(composer.m_TrackedObjectOffset.y, -500, 500);
+    }
+}
